6c0f8b1af1b78725c8d7d834619c5547a636c94e
Adrian0910 <uchihasantander@gmail.com>
2022-03-23 20:50:26 -0600

    estructura

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const fetchPokemon = () => {
     const pokeNameInput = document.getElementById("pokeName");
     let pokeName = pokeNameInput.value;
-    pokeName = pokeName.toLowerCase();
+    pokeName = pokeName.toLowerCase() 
     const url = `https://pokeapi.co/api/v2/pokemon/${pokeName}`;
     fetch(url).then((res) => {
         if (res.status != "200") {
-            console.log(res);
-            pokeImage("./pokemon-sad.gif")
+            pokeImage("./assets/pokemon-sad.gif");
         }
         else {
             return res.json();
